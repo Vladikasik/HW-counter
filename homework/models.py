9990d6c32c72fcb37fa44021a764b7840923527a
Vladislav <vsakayafignavlad@gmail.com>
2021-11-09 14:27:39 +0300

    hw model done

--- homework/models.py
+++ homework/models.py
@@ -5,10 +5,10 @@ class HomeWork(models.Model):
     id = models.PrimaryKeyField(primary_key=True)
     title = models.CharField(max_length=50)
     description = models.TextField()
+    task_file = models.URLField()
     priority = models.IntegerField(default=0)
     date_given = models.DateTimeField(auto_now=True)
     expiration_date = models.DateTimeField()
     result_in_system = models.BooleanField(default=False)
     link_to_result = models.URLField()
     result_text = models.TextField()
-    
